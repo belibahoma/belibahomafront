af56a4813c83e6eaa61d83f1df00d2983df8a0bc
matanyaglik <matanya.g@gmail.com>
2019-09-17 11:01:29 +0300

    fix relations

--- src/components/UI/Relations/Relations.js
+++ src/components/UI/Relations/Relations.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Table, Form } from "react-bootstrap";
 import config from "react-global-configuration";
 import axios from "axios";
 
 export default class Relations extends Component {
   state = {
     relationList: [],
-    userToken: ""
+    userToken: "",
+    showInActive: false
   };
 
   componentDidMount() {
@@ -48,73 +49,134 @@ export default class Relations extends Component {
 
   handleDelete = _id => {
     axios
-      .delete(`${config.get("serverAddress")}/api/relations/` + _id, null, {
+      .delete(`${config.get("serverAddress")}/api/relations/` + _id, {
         headers: { "x-auth-token": this.state.userToken }
       })
-      .then(res =>
+      .then(res => {
+        alert("הנתונים עודכנו בהצלחה");
+        let tmpRelations = this.state.relationList;
+        const relationToUpdateIndex = tmpRelations.findIndex(relation => {
+          return relation._id === _id;
+        });
+        tmpRelations[relationToUpdateIndex] = res.data;
         this.setState({
-          relationsList: this.state.relationList.filter(relation => {
-            return relation._id !== _id;
-          })
-        })
+          relationList: tmpRelations
+        });
+      })
+      .catch(err => {
+        alert(`${err.message} ${err.response ? ": " + err.response.data : ""}`);
+      });
+  };
+
+  handleActive = _id => {
+    let relationToUpdate = this.state.relationList.find(relation => {
+      return relation._id === _id;
+    });
+    relationToUpdate.isActive = true;
+    axios
+      .put(
+        `${config.get("serverAddress")}/api/relations/` + _id,
+        relationToUpdate,
+        {
+          headers: { "x-auth-token": this.state.userToken }
+        }
       )
+      .then(res => {
+        alert("הנתונים עודכנו בהצלחה");
+        let tmpRelations = this.state.relationList;
+        const relationToUpdateIndex = tmpRelations.findIndex(relation => {
+          return relation._id === _id;
+        });
+        tmpRelations[relationToUpdateIndex] = res.data;
+        this.setState({
+          relationList: tmpRelations
+        });
+      })
       .catch(err => {
         alert(`${err.message} ${err.response ? ": " + err.response.data : ""}`);
       });
   };
 
   relationRows = () => {
-    return this.state.relationList.map(relation => {
-      return (
-        <tr key={relation._id}>
-          <td>
-            <Button
-              className="btn btn-primary"
-              onClick={() => {
-                this.handleReport(relation);
-              }}
-            >
-              עבור לדיווחים
-            </Button>
-          </td>
-          <td>{relation.tutor_id.fname}</td>
-          <td>{relation.tutor_id.lname}</td>
-          <td>{relation.trainee_id.fname}</td>
-          <td>{relation.trainee_id.lname}</td>
-          <td>
-            <Button
-              className="btn btn-danger"
-              onClick={() => {
-                this.handleDelete(relation._id);
-              }}
-            >
-              הסר
-            </Button>
-          </td>
-        </tr>
-      );
-    });
+    return this.state.relationList
+      .filter(relation => {
+        return relation.isActive || this.state.showInActive;
+      })
+      .map(relation => {
+        return (
+          <tr key={relation._id}>
+            <td>
+              <Button
+                className="btn btn-primary"
+                onClick={() => {
+                  this.handleReport(relation);
+                }}
+              >
+                עבור לדיווחים
+              </Button>
+            </td>
+            <td>{relation.tutor_id.fname}</td>
+            <td>{relation.tutor_id.lname}</td>
+            <td>{relation.trainee_id.fname}</td>
+            <td>{relation.trainee_id.lname}</td>
+            <td>
+              {relation.isActive ? (
+                <Button
+                  className="btn btn-danger"
+                  onClick={() => {
+                    this.handleDelete(relation._id);
+                  }}
+                >
+                  הפוך ללא פעיל
+                </Button>
+              ) : (
+                <Button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    this.handleActive(relation._id);
+                  }}
+                >
+                  הפוך לפעיל
+                </Button>
+              )}
+            </td>
+          </tr>
+        );
+      });
   };
 
   render() {
     return (
-      <Table
-        striped
-        bordered
-        hover
-        dir="rtl"
-        className="text-right align-items-center"
-      >
-        <tr>
-          <th />
-          <th>שם פרטי מתגבר</th>
-          <th>שם משפחה מתגבר</th>
-          <th>שם פרטי מתגבר</th>
-          <th>שם משפחה מתגבר</th>
-          <th>הסר</th>
-        </tr>
-        <tbody>{this.relationRows()}</tbody>
-      </Table>
+      <React.Fragment>
+        <Form className="text-right">
+          <Form.Label dir="rtl" className="h6 text-danger">
+            האם להציג קשרים לא פעילים?
+          </Form.Label>
+          <Form.Check
+            checked={this.state.showInActive}
+            onChange={() => {
+              this.setState({ showInActive: !this.state.showInActive });
+            }}
+          ></Form.Check>
+        </Form>
+        <Table
+          striped
+          bordered
+          hover
+          dir="rtl"
+          className="text-right align-items-center"
+        >
+          <tr>
+            <th />
+            <th>שם פרטי מתגבר</th>
+            <th>שם משפחה מתגבר</th>
+            <th>שם פרטי מתגבר</th>
+            <th>שם משפחה מתגבר</th>
+            <th>הסר</th>
+          </tr>
+          <tbody>{this.relationRows()}</tbody>
+        </Table>
+      </React.Fragment>
     );
   }
 }
