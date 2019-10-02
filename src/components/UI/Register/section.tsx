e24bb168ca3779da27acbe033f8fb7cce8b9828f
shimonberko <shimon.berkovich@mail.huji.ac.il>
2019-10-02 22:35:39 +0300

    change dependency

--- src/components/UI/Register/section.tsx
+++ src/components/UI/Register/section.tsx
@@ -1,5 +1,5 @@
 import  '../../../section.css';
-import {Component} from "react";
+import React from "react";
 import { visible } from 'ansi-colors';
 // import { FontIcon } from 'material-ui';
 
@@ -17,7 +17,7 @@ interface State{
 }
 
 
-export default class Section extends Component<Props, State>{
+export default class Section extends React.Component<Props, State>{
 	constructor(props: Props){
 		super(props);
 		console.log(props.display);
